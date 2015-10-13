627ee868fdb3ea58db757a3bf25e69678ad750ff
Jon Haddad <jon@jonhaddad.com>
2015-10-13 10:46:40 -0700

    fixed build for mac users, fixed typo in description

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 fn main() {
-    println!("cargo:rustc-flags=-l dylib=ssl");    
+    println!("cargo:rustc-flags=-l dylib=ssl");
     println!("cargo:rustc-flags=-l dylib=stdc++");
-    println!("cargo:rustc-flags=-l dylib=uv");    
-    println!("cargo:rustc-flags=-l dylib=crypto");    
-    println!("cargo:rustc-flags=-l dylib=cassandra");    
+    println!("cargo:rustc-flags=-l dylib=uv");
+
+    println!("cargo:rustc-flags=-l dylib=crypto");
+    println!("cargo:rustc-flags=-l dylib=cassandra");
+
     println!("cargo:rustc-link-search={}", "/usr/lib/");
+
+    // on osx, libuv lives in /usr/local/lib
+    println!("cargo:rustc-link-search={}", "/usr/local/lib/");
+    // on osx, cpp driver is installed to /usr/local/lib64
+    println!("cargo:rustc-link-search={}", "/usr/local/lib64/");
 //    println!("cargo:rustc-link-lib=static=cassandra_static");
 }
