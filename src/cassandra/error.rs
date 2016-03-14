cassandra-rs/cassandra-rs

commit 08a20f23e4f8c588cb91e3b9399db4bdbfe4c51e

made even clippy_pedantic happy

Cargo.toml
@@ -6,7 +6,7 @@ license = "Apache-2.0"
 homepage = "https://github.com/tupshin/cassandra-rust"
 repository = "https://github.com/tupshin/cassandra-rust"
 name = "cassandra"
-version = "0.6.3"
+version = "0.6.4"
 authors = ["Tupshin Harper <tupshin@tupshin.com>"]
 build = "build.rs"
 

src/cassandra/error.rs
@@ -247,6 +247,7 @@ impl CassErrorResult {
     ///   <li>CASS_ERROR_SERVER_ALREADY_EXISTS</li>
     ///   <li>CASS_ERROR_SERVER_FUNCTION_FAILURE</li>
     ///</ul>
+    #[allow(cast_possible_truncation)]
     pub fn keyspace(&self) -> String {
         unsafe {
             let mut name = mem::zeroed();
@@ -254,7 +255,7 @@ impl CassErrorResult {
             match cass_error_result_keyspace(self.0, &mut name, &mut length) {
                 CASS_OK => {
                     let slice = slice::from_raw_parts(name as *const u8, length as usize);
-                    str::from_utf8(slice).unwrap().to_owned()
+                    str::from_utf8(slice).expect("must be utf8").to_owned()
                 }
                 err => panic!("impossible: {:?}", err),
             }
@@ -263,6 +264,7 @@ impl CassErrorResult {
 
     ///Gets the affected table for the already exists error
     ///(CASS_ERROR_SERVER_ALREADY_EXISTS) result type.
+    #[allow(cast_possible_truncation)]
     pub fn table(&self) -> String {
         unsafe {
             let mut name = mem::zeroed();
@@ -270,7 +272,7 @@ impl CassErrorResult {
             match cass_error_result_table(self.0, &mut name, &mut length) {
                 CASS_OK => {
                     let slice = slice::from_raw_parts(name as *const u8, length as usize);
-                    str::from_utf8(slice).unwrap().to_owned()
+                    str::from_utf8(slice).expect("must be utf8").to_owned()
                 }
                 err => panic!("impossible: {:?}", err),
             }
@@ -279,6 +281,7 @@ impl CassErrorResult {
 
     ///Gets the affected function for the function failure error
     ///(CASS_ERROR_SERVER_FUNCTION_FAILURE) result type.
+    #[allow(cast_possible_truncation)]
     pub fn function(&self) -> String {
         unsafe {
             let mut name = mem::zeroed();
@@ -286,7 +289,7 @@ impl CassErrorResult {
             match cass_error_result_function(self.0, &mut name, &mut length) {
                 CASS_OK => {
                     let slice = slice::from_raw_parts(name as *const u8, length as usize);
-                    str::from_utf8(slice).unwrap().to_owned()
+                    str::from_utf8(slice).expect("must be utf8").to_owned()
                 }
                 err => panic!("impossible: {:?}", err),
             }
@@ -303,7 +306,7 @@ impl CassErrorResult {
     ///error (CASS_ERROR_SERVER_FUNCTION_FAILURE) result type.
     pub fn arg_type(&self, index: u64, arg_type: &str) -> u32 {
         unsafe {
-            let cstr = CString::new(arg_type).unwrap();
+            let cstr = CString::new(arg_type).expect("must be utf8");
             cass_error_result_arg_type(self.0,
                                        index,
                                        &mut cstr.as_ptr(),

src/cassandra/uuid.rs
@@ -107,7 +107,7 @@ impl str::FromStr for Uuid {
     fn from_str(str: &str) -> Result<Uuid, CassError> {
         unsafe {
             let mut uuid = mem::zeroed();
-            match cass_uuid_from_string(CString::new(str).unwrap().as_ptr(), &mut uuid) {
+            match cass_uuid_from_string(CString::new(str).expect("must be utf8").as_ptr(), &mut uuid) {
                 CASS_OK => Ok(Uuid(uuid)),
                 err => Err(CassError::build(err)),
             }

src/cassandra/value.rs
@@ -136,14 +136,14 @@ impl Debug for Value {
                 CASS_VALUE_TYPE_UUID => write!(f, "UUID: {}", self.get_uuid().unwrap()),
                 CASS_VALUE_TYPE_SET | CASS_VALUE_TYPE_LIST => {
                     try!(write!(f, "["));
-                    for item in self.get_set().unwrap() {
+                    for item in self.get_set().expect("set must be a set") {
                         try!(write!(f, "SET {:?} ", item))
                     }
                     try!(write!(f, "]"));
                     Ok(())
                 }
                 CASS_VALUE_TYPE_MAP => {
-                    for item in self.get_map().unwrap() {
+                    for item in self.get_map().expect("map must be a map") {
                         try!(write!(f, "MAP {:?}:{:?}", item.0, item.1))
                     }
                     Ok(())
@@ -185,20 +185,20 @@ impl Display for Value {
                 CASS_VALUE_TYPE_TIMEUUID => write!(f, "TIMEUUID: {}", self.get_uuid().unwrap()),
                 CASS_VALUE_TYPE_SET => {
                     try!(write!(f, "["));
-                    for item in self.get_set().unwrap() {
+                    for item in self.get_set().expect("set must be a set") {
                         try!(write!(f, "{} ", item))
                     }
                     try!(write!(f, "]"));
                     Ok(())
                 }
                 CASS_VALUE_TYPE_MAP => {
-                    for item in self.get_map().unwrap() {
+                    for item in self.get_map().expect("map must be a map") {
                         try!(write!(f, "MAP {}:{}", item.0, item.1))
                     }
                     Ok(())
                 }
                 // FIXME
-                err => write!(f, "{:?}", err),
+                _ => write!(f, "unknown type"),
             }
         }
     }
@@ -237,6 +237,7 @@ impl Value {
     //    }
 
     /// Gets the name of the keyspace.
+    #[allow(cast_possible_truncation)]
     pub fn get_bytes(&self) -> Result<&[u8], CassError> {
         unsafe {
             let mut output = mem::zeroed();
@@ -318,7 +319,7 @@ impl Value {
     //                _ => Err(CassError::build(CassErrorTypes::LIB_INVALID_VALUE_TYPE as u32)),
     //            }
     //        }
-    //    } 
+    //    }
 
 
     // ~ pub fn map_iter(&self) -> Result<MapIterator,CassError> {unsafe{
@@ -333,16 +334,16 @@ impl Value {
     // ~ }}
 
     ///Get this value as a string
+    #[allow(cast_possible_truncation)]
     pub fn get_string(&self) -> Result<String, CassError> {
         unsafe {
             let message: CString = mem::zeroed();
-            let mut message = message.as_ptr();
             let mut message_length = mem::zeroed();
-            cass_value_get_string(self.0, &mut message, &mut (message_length));
+            cass_value_get_string(self.0, &mut message.as_ptr(), &mut (message_length));
 
-            let slice = slice::from_raw_parts(message as *const u8, message_length as usize);
-            let err = CassError::build(cass_value_get_string(self.0, &mut message, &mut (message_length)));
-            err.wrap(str::from_utf8(slice).unwrap().to_owned())
+            let slice = slice::from_raw_parts(message.as_ptr() as *const u8, message_length as usize);
+            let err = CassError::build(cass_value_get_string(self.0, &mut message.as_ptr(), &mut (message_length)));
+            err.wrap(str::from_utf8(slice).expect("must be utf8").to_owned())
         }
     }
 

src/cassandra/cluster.rs
@@ -81,14 +81,11 @@ impl fmt::Display for ContactPoints {
 impl FromStr for ContactPoints {
     type Err = AddrParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let contact_points: Vec<&str> = s.split(",").collect();
+        let points: Vec<&str> = s.split(',').collect();
 
-        let contact_points = contact_points.iter()
-                                           .map(|addr| {
-                                               println!("{}", addr);
-                                               Ipv4Addr::from_str(addr)
-                                           })
-                                           .collect();
+        let contact_points = points.iter()
+                                   .map(|addr| Ipv4Addr::from_str(addr))
+                                   .collect();
         Ok(ContactPoints(try!(contact_points)))
     }
 }
@@ -110,7 +107,6 @@ pub struct Cluster(pub *mut _Cluster);
 impl Drop for Cluster {
     ///Frees a cluster instance.
     fn drop(&mut self) {
-    	println!("dropping cluster");
         unsafe { cass_cluster_free(self.0) }
     }
 }
@@ -141,10 +137,8 @@ impl Cluster {
     /// ```
     ///
     pub fn set_contact_points<T: Into<ContactPoints>>(&mut self, contact_points: T) -> Result<&mut Self, CassError> {
-        let contact_points = contact_points.into();
         unsafe {
-            println!("p:{}:", contact_points);
-            let s = CString::new(contact_points.to_string()).unwrap();
+            let s = CString::new(contact_points.into().to_string()).expect("must be utf8");
             let err = CassError::build(cass_cluster_set_contact_points(self.0, s.as_ptr()));
             err.wrap(self)
         }
@@ -334,6 +328,7 @@ impl Cluster {
     ///
     /// ```
     /// Default: 5000ms
+    #[allow(cast_possible_truncation,cast_sign_loss)]
     pub fn set_connect_timeout(&mut self, timeout: Duration) -> &Self {
         unsafe {
             cass_cluster_set_connect_timeout(self.0, timeout.num_milliseconds() as u32);
@@ -345,6 +340,7 @@ impl Cluster {
     ///
     /// ```
     /// Default: 12000ms
+    #[allow(cast_possible_truncation,cast_sign_loss)]
     pub fn set_request_timeout(&mut self, timeout: Duration) -> &Self {
         unsafe {
             cass_cluster_set_request_timeout(self.0, timeout.num_milliseconds() as u32);
@@ -389,7 +385,7 @@ impl Cluster {
                                         -> Result<&Self, CassError> {
         unsafe {
             CassError::build({
-                let local_dc = CString::new(local_dc).unwrap();
+                let local_dc = CString::new(local_dc).expect("must be utf8");
                 cass_cluster_set_load_balance_dc_aware(self.0,
                                                        local_dc.as_ptr(),
                                                        used_hosts_per_remote_dc,
@@ -448,6 +444,7 @@ impl Cluster {
     ///  <li>update_rate_ms: 100 milliseconds</li>
     ///  <li>min_measured: 50</li>
     ///</ul>
+    #[allow(cast_sign_loss)]
     pub fn set_latency_aware_routing_settings(&mut self, exclusion_threshold: f64, scale: Duration,
         retry_period: Duration, update_rate: Duration, min_measured: u64)
                                               -> &Self {
@@ -496,6 +493,7 @@ impl Cluster {
     ///
     /// ```
     ///Default: false (disabled).
+    #[allow(cast_possible_truncation,cast_sign_loss)]
     pub fn set_tcp_keepalive(&mut self, enable: bool, delay: Duration) -> &Self {
         unsafe {
             cass_cluster_set_tcp_keepalive(self.0,
@@ -524,6 +522,7 @@ impl Cluster {
     ///
     /// ````
     // Default: 30 seconds
+    #[allow(cast_possible_truncation,cast_sign_loss)]
     pub fn set_connection_heartbeat_interval(&mut self, hearbeat: Duration) -> &mut Self {
         unsafe {
             cass_cluster_set_connection_heartbeat_interval(self.0, hearbeat.num_seconds() as u32);
@@ -536,6 +535,7 @@ impl Cluster {
     ///
     /// ```
     ///Default: 60 seconds
+    #[allow(cast_possible_truncation,cast_sign_loss)]
     pub fn set_connection_idle_timeout(&mut self, timeout: Duration) -> &mut Self {
         unsafe {
             cass_cluster_set_connection_idle_timeout(self.0, timeout.num_seconds() as u32);

src/cassandra/collection.rs
@@ -214,7 +214,7 @@ impl CassCollection for List {
     ///Appends an "ascii", "text" or "varchar" to the collection.
     fn append_string(&mut self, value: &str) -> Result<&Self, CassError> {
         unsafe {
-            let cstr = CString::new(value).unwrap();
+            let cstr = CString::new(value).expect("must be utf8");
             let result = cass_collection_append_string(self.inner(), cstr.as_ptr());
             CassError::build(result).wrap(self)
         }
@@ -341,7 +341,7 @@ impl CassCollection for Set {
     ///Appends an "ascii", "text" or "varchar" to the collection.
     fn append_string(&mut self, value: &str) -> Result<&Self, CassError> {
         unsafe {
-            let cstr = CString::new(value).unwrap();
+            let cstr = CString::new(value).expect("must be utf8");
             let result = cass_collection_append_string(self.inner(), cstr.as_ptr());
             CassError::build(result).wrap(self)
         }
@@ -465,7 +465,7 @@ impl CassCollection for Map {
     ///Appends an "ascii", "text" or "varchar" to the collection.
     fn append_string(&mut self, value: &str) -> Result<&Self, CassError> {
         unsafe {
-            let cstr = CString::new(value).unwrap();
+            let cstr = CString::new(value).expect("must be utf8");
             let result = cass_collection_append_string(self.inner(), cstr.as_ptr());
             CassError::build(result).wrap(self)
         }

src/cassandra/column.rs
@@ -125,19 +125,19 @@ impl Debug for Column {
             CASS_VALUE_TYPE_TIMEUUID => write!(f, "TIMEUUID Cassandra type"),
             CASS_VALUE_TYPE_INET => write!(f, "INET Cassandra type"),
             CASS_VALUE_TYPE_LIST => {
-                for item in self.set_iter().unwrap() {
+                for item in self.set_iter().expect("a list should always be able to return a set iterator") {
                     try!(write!(f, "LIST {:?}", item))
                 }
                 Ok(())
             }
             CASS_VALUE_TYPE_MAP => {
-                for item in self.map_iter().unwrap() {
+                for item in self.map_iter().expect("a map should always be able to return a map iterator") {
                     try!(write!(f, "LIST {:?}", item))
                 }
                 Ok(())
             }
             CASS_VALUE_TYPE_SET => {
-                for item in self.set_iter().unwrap() {
+                for item in self.set_iter().expect("a set must be able to return a set iterator") {
                     try!(write!(f, "SET {:?}", item))
                 }
                 Ok(())
@@ -175,20 +175,20 @@ impl Display for Column {
             CASS_VALUE_TYPE_TIMEUUID => write!(f, "TIMEUUID Cassandra type"),
             CASS_VALUE_TYPE_INET => write!(f, "INET Cassandra type"),
             CASS_VALUE_TYPE_LIST => {
-                for item in self.set_iter().unwrap() {
-                    try!(write!(f, "LIST {:?}", item))
+                for item in self.set_iter().expect("a list must be able to return a set iterator") {
+                    try!(write!(f, "LIST {}", item))
                 }
                 Ok(())
             }
             CASS_VALUE_TYPE_MAP => {
-                for item in self.map_iter().unwrap() {
-                    try!(write!(f, "LIST {:?}", item))
+                for item in self.map_iter().expect("a map must be able to return a map iterator") {
+                    try!(write!(f, "MAP {}:{}", item.0, item.1))
                 }
                 Ok(())
             }
             CASS_VALUE_TYPE_SET => {
-                for item in self.set_iter().unwrap() {
-                    try!(write!(f, "SET {:?}", item))
+                for item in self.set_iter().expect("a set must be able to return a set iterator") {
+                    try!(write!(f, "SET {}", item))
                 }
                 Ok(())
             }
@@ -259,6 +259,7 @@ impl Column {
     //    }
 
     ///Gets the string from this column or errors if you ask for the wrong type
+    #[allow(cast_possible_truncation)]
     pub fn get_string(&self) -> Result<String, CassError> {
         unsafe {
             match cass_value_type(self.0) {
@@ -268,7 +269,7 @@ impl Column {
                     match cass_value_get_string(self.0, &mut message, &mut message_length) {
                         CASS_OK => {
                             let slice = slice::from_raw_parts(message as *const u8, message_length as usize);
-                            Ok(str::from_utf8(slice).unwrap().to_owned())
+                            Ok(str::from_utf8(slice).expect("must be utf8").to_owned())
                         }
                         err => Err(CassError::build(err)),
                     }

src/cassandra/data_type.rs
@@ -89,10 +89,10 @@ impl DataType {
     pub fn type_name<S>(data_type: DataType, type_name: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let type_name = CString::new(type_name.into()).unwrap();
+            let type_name2 = CString::new(type_name.into()).expect("must be valid utf8");
             CassError::build(cass_data_type_type_name(data_type.0,
-                                                      &mut type_name.as_ptr(),
-                                                      &mut (type_name.as_bytes().len() as u64)))
+                                                      &mut type_name2.as_ptr(),
+                                                      &mut (type_name2.as_bytes().len() as u64)))
                 .wrap(())
         }
     }
@@ -103,8 +103,11 @@ impl DataType {
     pub fn set_type_name<S>(data_type: DataType, type_name: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let type_name = CString::new(type_name.into()).unwrap();
-            CassError::build(cass_data_type_set_type_name(data_type.0, type_name.as_ptr())).wrap(())
+            CassError::build(cass_data_type_set_type_name(data_type.0,
+                                                          CString::new(type_name.into())
+                                                              .expect("must be utf8")
+                                                              .as_ptr()))
+                .wrap(())
         }
     }
 
@@ -114,10 +117,10 @@ impl DataType {
     pub fn keyspace<S>(data_type: DataType, keyspace: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let keyspace = CString::new(keyspace.into()).unwrap();
+            let keyspace2 = CString::new(keyspace.into()).expect("must be utf8");
             CassError::build(cass_data_type_keyspace(data_type.0,
-                                                     &mut (keyspace.as_ptr()),
-                                                     &mut (keyspace.as_bytes().len() as u64)))
+                                                     &mut (keyspace2.as_ptr()),
+                                                     &mut (keyspace2.as_bytes().len() as u64)))
                 .wrap(())
         }
     }
@@ -128,8 +131,9 @@ impl DataType {
     pub fn set_keyspace<S>(data_type: DataType, keyspace: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let keyspace = CString::new(keyspace.into()).unwrap();
-            CassError::build(cass_data_type_set_keyspace(data_type.0, keyspace.as_ptr())).wrap(())
+            CassError::build(cass_data_type_set_keyspace(data_type.0,
+                                                         CString::new(keyspace.into()).expect("must be utf8").as_ptr()))
+                .wrap(())
         }
     }
 
@@ -139,10 +143,10 @@ impl DataType {
     pub fn class_name<S>(data_type: DataType, class_name: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let class_name = CString::new(class_name.into()).unwrap();
+            let class_name2 = CString::new(class_name.into()).expect("must be valid utf8");
             CassError::build(cass_data_type_class_name(data_type.0,
-                                                       &mut class_name.as_ptr(),
-                                                       &mut (class_name.as_bytes().len() as u64)))
+                                                       &mut class_name2.as_ptr(),
+                                                       &mut (class_name2.as_bytes().len() as u64)))
                 .wrap(())
         }
     }
@@ -153,8 +157,11 @@ impl DataType {
     pub fn set_class_name<S>(&self, class_name: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let class_name = CString::new(class_name.into()).unwrap();
-            CassError::build(cass_data_type_set_class_name(self.0, class_name.as_ptr())).wrap(())
+            CassError::build(cass_data_type_set_class_name(self.0,
+                                                           CString::new(class_name.into())
+                                                               .expect("must be valid utf8")
+                                                               .as_ptr()))
+                .wrap(())
         }
     }
 
@@ -180,8 +187,10 @@ impl DataType {
     pub fn sub_data_type_by_name<S>(data_type: DataType, name: S) -> ConstDataType
         where S: Into<String> {
         unsafe {
-            let name = CString::new(name.into()).unwrap();
-            ConstDataType(cass_data_type_sub_data_type_by_name(data_type.0, name.as_ptr()))
+            ConstDataType(cass_data_type_sub_data_type_by_name(data_type.0,
+                                                               CString::new(name.into())
+                                                                   .expect("must be utf8")
+                                                                   .as_ptr()))
         }
     }
 
@@ -191,11 +200,11 @@ impl DataType {
     pub fn sub_type_name<S>(data_type: DataType, index: u64, name: S) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let name = CString::new(name.into()).unwrap();
+            let name2 = CString::new(name.into()).expect("must be utf8");
             CassError::build(cass_data_type_sub_type_name(data_type.0,
                                                           index,
-                                                          &mut name.as_ptr(),
-                                                          &mut (name.as_bytes().len() as u64)))
+                                                          &mut name2.as_ptr(),
+                                                          &mut (name2.as_bytes().len() as u64)))
                 .wrap(())
         }
     }
@@ -213,8 +222,12 @@ impl DataType {
     pub fn add_sub_type_by_name<S>(&mut self, name: S, sub_data_type: DataType) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let name = CString::new(name.into()).unwrap();
-            CassError::build(cass_data_type_add_sub_type_by_name(self.0, name.as_ptr(), sub_data_type.0)).wrap(())
+            CassError::build(cass_data_type_add_sub_type_by_name(self.0,
+                                                                 CString::new(name.into())
+                                                                     .expect("must be utf8")
+                                                                     .as_ptr(),
+                                                                 sub_data_type.0))
+                .wrap(())
         }
     }
 
@@ -232,8 +245,12 @@ impl DataType {
     pub fn add_sub_value_type_by_name<S>(&self, name: &str, typ: ValueType) -> Result<(), CassError>
         where S: Into<String> {
         unsafe {
-            let name = CString::new(name).unwrap();
-            CassError::build(cass_data_type_add_sub_value_type_by_name(self.0, name.as_ptr(), typ.inner())).wrap(())
+            CassError::build(cass_data_type_add_sub_value_type_by_name(self.0,
+                                                                       CString::new(name)
+                                                                           .expect("must be utf8")
+                                                                           .as_ptr(),
+                                                                       typ.inner()))
+                .wrap(())
         }
 
     }

commit 69b4c5e90a8ed95abbc6f7f74f17c41e8bc16d20

fixed segfault introduced by 08a20f23e4f8c588cb91e3b9399db4bdbfe4c51e

.gitignore
@@ -10,3 +10,7 @@ Cargo.lock
 
 # Generated by Cargo
 /target/
+
+.project
+.settings/
+.travis.yml

Cargo.toml
@@ -6,7 +6,7 @@ license = "Apache-2.0"
 homepage = "https://github.com/tupshin/cassandra-rust"
 repository = "https://github.com/tupshin/cassandra-rust"
 name = "cassandra"
-version = "0.6.7"
+version = "0.6.8"
 authors = ["Tupshin Harper <tupshin@tupshin.com>"]
 build = "build.rs"
 

src/cassandra/error.rs
@@ -331,7 +331,7 @@ impl CassError {
     ///Gets the textual description for this error
     pub fn desc(&self) -> &str {
         {
-            CassError::pointer_to_string(unsafe { cass_error_desc(self.0) } )
+            CassError::pointer_to_string(unsafe { cass_error_desc(self.0) })
             //            match *self {
             //                CassError::Lib(ref err) => CassError::pointer_to_string(cass_error_desc(err.err)),
             //                CassError::Ssl(ref err) => CassError::pointer_to_string(cass_error_desc(err.0)),

src/cassandra/uuid.rs
@@ -62,9 +62,9 @@ impl Drop for UuidGen {
 }
 
 impl Debug for Uuid {
-   fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-       fmt::Display::fmt(self, f)
-   }
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
 }
 
 impl Display for Uuid {
@@ -76,8 +76,8 @@ impl Display for Uuid {
             cass_uuid_string(self.0, cstr); // Write the UUID to *c_char
             buf = CString::from_raw(cstr); // Convert from *c_char back to a CString.
             let str = match buf.into_string() {
-              Ok(s) => s,
-              Err(_) => return Err(fmt::Error)
+                Ok(s) => s,
+                Err(_) => return Err(fmt::Error),
             };
             fmt::Display::fmt(&str, f)
         }

src/cassandra/value.rs
@@ -338,12 +338,17 @@ impl Value {
     pub fn get_string(&self) -> Result<String, CassError> {
         unsafe {
             let message: CString = mem::zeroed();
+            let mut message_ptr = message.as_ptr();
             let mut message_length = mem::zeroed();
-            cass_value_get_string(self.0, &mut message.as_ptr(), &mut (message_length));
+            cass_value_get_string(self.0, &mut message_ptr, &mut (message_length));
 
-            let slice = slice::from_raw_parts(message.as_ptr() as *const u8, message_length as usize);
-            let err = CassError::build(cass_value_get_string(self.0, &mut message.as_ptr(), &mut (message_length)));
-            err.wrap(str::from_utf8(slice).expect("must be utf8").to_owned())
+            let slice = slice::from_raw_parts(message_ptr as *const u8, message_length as usize);
+            let err = CassError::build(cass_value_get_string(self.0, &mut message_ptr, &mut (message_length)));
+            err.wrap(str::from_utf8(slice).unwrap().to_owned())
+
+            //            let slice = slice::from_raw_parts(message.as_ptr() as *const u8, message_length as usize);
+            //            let err = CassError::build(cass_value_get_string(self.0, &mut message.as_ptr(), &mut (message_length)));
+            //            err.wrap(str::from_utf8(slice).expect("must be utf8").to_owned())
         }
     }
 