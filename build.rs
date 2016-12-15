4e6b4d9bceff3f80d9abeef3b6667ad8bad1adbb
Tupshin Harper <tupshin@tupshin.com>
2016-12-15 08:11:48 -0500

    making build match cassandra-sys

--- build.rs
+++ build.rs
@@ -1,24 +1,20 @@
 extern crate cheddar;
 
 fn main() {
+    if let Some(datastax_dir) = option_env!("CASSANDRA_SYS_LIB_PATH") {
+        for p in datastax_dir.split(";") {
+            println!("cargo:rustc-link-search={}", p);
+        }
+    }
+    println!("cargo:rustc-flags=-l dylib=crypto");
     println!("cargo:rustc-flags=-l dylib=ssl");
     println!("cargo:rustc-flags=-l dylib=stdc++");
     println!("cargo:rustc-flags=-l dylib=uv");
-
-    println!("cargo:rustc-flags=-l dylib=crypto");
-    println!("cargo:rustc-flags=-l dylib=cassandra");
-
+    println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-search={}", "/usr/local/lib/x86_64-linux-gnu");
+    println!("cargo:rustc-link-search={}", "/usr/local/lib64");
+    println!("cargo:rustc-link-search={}", "/usr/local/lib");
+    println!("cargo:rustc-link-search={}", "/usr/lib64/");
     println!("cargo:rustc-link-search={}", "/usr/lib/");
-
-    // the debian binary packages of the cpp driver install to /usr/lib/x86_64-linux-gnu/
-    println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu/");
-
-    // on osx, libuv lives in /usr/local/lib
-    println!("cargo:rustc-link-search={}", "/usr/local/lib/");
-
-    // on osx, cpp driver is installed to /usr/local/lib64
-    println!("cargo:rustc-link-search={}", "/usr/local/lib64/");
-    // println!("cargo:rustc-link-lib=static=cassandra_static");
-    //    cheddar::Cheddar::new().unwrap()
-    //        .run_build("target/include/cassandra_rs.h");
+    println!("cargo:rustc-link-lib=static=cassandra_static");
 }
