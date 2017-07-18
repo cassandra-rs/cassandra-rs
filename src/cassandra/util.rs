809d5029fc640b6dc5f060352e0c691e85cda52f
Keith Wansbrough <Keith.Wansbrough@metaswitch.com>
2017-07-18 17:28:08 +0100

    Restrict access to cassandra-sys types.
    Callers outside this crate should not be able to access the inner types,
    lest we leak implementation details.
    
    Signed-off-by: Keith Wansbrough <Keith.Wansbrough@metaswitch.com>

--- src/cassandra/util.rs
+++ src/cassandra/util.rs
@@ -1,4 +1,7 @@
-pub trait Protected<T> {
+/// Interconvert between external and internal representation.
+/// We can freely do this within this crate, but must not allow
+/// our clients to do this.
+pub(crate) trait Protected<T> {
     fn build(inner: T) -> Self;
     fn inner(&self) -> T;
 }
