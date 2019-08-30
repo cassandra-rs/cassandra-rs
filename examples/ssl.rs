adf0a16ace0d6b9f3c166aeaa313e0fd5d98ab88
Keith Wansbrough <Keith.Wansbrough@metaswitch.com>
2019-08-30 08:32:47 +0100

    Remove unnecessary use of Option.
    Signed-off-by: Keith.Wansbrough@metaswitch.com

--- examples/ssl.rs
+++ examples/ssl.rs
@@ -8,51 +8,40 @@ use std::fs;
 fn main() {
     let query = stmt!("SELECT keyspace_name FROM system_schema.keyspaces;");
     let col_name = "keyspace_name";
-
     let contact_points = "127.0.0.1";
-    let tls_ca_certificate_path = Some("ca/certificate/path");
+    let tls_ca_certificate_path = "ca/certificate/path";
 
-    let ca_cert = match &tls_ca_certificate_path.clone() {
-        Some(path) => match fs::read_to_string(path) {
-            Ok(cert) => {
-                println!("Read in certificate file");
-                Some(cert)
-            }
-            Err(e) => {
-                panic!("Failed to open certificate file. Error: {}", e);
-            }
-        },
-        None => None,
-    };
-
-    let mut sslopt: Option<cassandra_cpp::Ssl> = None;
-    if let Some(cert) = ca_cert.clone() {
-        let mut ssl = cassandra_cpp::Ssl::default();
-        match cassandra_cpp::Ssl::add_trusted_cert(&mut ssl, &cert) {
-            Ok(_o) => {
-                println!("Added trusted certificate");
-            }
-            Err(e) => {
-                panic!("Failed to add trusted certificate. Error: {}", e);
-            }
+    let ca_cert = match fs::read_to_string(tls_ca_certificate_path) {
+        Ok(cert) => {
+            println!("Read in certificate file");
+            cert
         }
-        sslopt = Some(ssl);
+        Err(e) => {
+            panic!("Failed to open certificate file. Error: {}", e);
+        }
     };
 
-    if let Some(ref mut ssl) = sslopt {
-        println!("Set verification level");
-        let verify_level = vec![cassandra_cpp::SslVerifyFlag::PEER_IDENTITY];
-            cassandra_cpp::Ssl::set_verify_flags(ssl, &verify_level);
+    let mut ssl = cassandra_cpp::Ssl::default();
+
+    match cassandra_cpp::Ssl::add_trusted_cert(&mut ssl, &ca_cert) {
+        Ok(_o) => {
+            println!("Added trusted certificate");
+        }
+        Err(e) => {
+            panic!("Failed to add trusted certificate. Error: {}", e);
+        }
     }
+
+    println!("Set verification level");
+    let verify_level = vec![cassandra_cpp::SslVerifyFlag::PEER_IDENTITY];
+    cassandra_cpp::Ssl::set_verify_flags(&mut ssl, &verify_level);
 
     let mut cluster = Cluster::default();
     cluster.set_contact_points(contact_points).unwrap();
     cluster.set_load_balance_round_robin();
 
-    if let Some(ref mut ssl) = sslopt {
-                                    println!("Adding SSL to cluster");
-                                    cluster.set_ssl(ssl);
-                                }
+    println!("Adding SSL to cluster");
+    cluster.set_ssl(&mut ssl);
 
     match cluster.connect() {
         Ok(ref mut session) => {
@@ -66,4 +55,3 @@ fn main() {
         err => println!("{:?}", err),
     }
 }
-
